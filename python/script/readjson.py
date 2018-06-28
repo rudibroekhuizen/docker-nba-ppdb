1ec122a70eeab2ffeb8eb58b35ac625e01fc2cb1
Joep Vermaat <joep@joep-i.nl>
2018-06-28 08:28:31 +0200

    Changes!

--- python/script/readjson.py
+++ python/script/readjson.py
@@ -18,55 +18,73 @@ fh.setFormatter(formatter)
 fh.setLevel(logging.DEBUG)
 logger.addHandler(fh)
 
+class JsonImporter():
 
-@db_session
-def parse_json(filename):
-    counter = {'lines':0, 'new':0, 'updated':0, 'skipped':0}
-    start = timer()
-    with open(filename, 'r') as f:
-        for line in f:
-            counter['lines'] += 1
-            rec = json.loads(line.strip())
-            json_hash = md5(line.encode('utf-8').strip()).hexdigest()
-            scigroup = rec['identifications'][0]['scientificName']['scientificNameGroup']
-            d = Document.get(record_id = rec['unitID'])
-            if (d):
-                if (json_hash != d.hash) :
-                    d.hash = json_hash
-                    d.source = rec['sourceSystem']['code']
-                    d.record = line
-                    d.status = 'Updated'
-                    d.nds_index = 'specimen'
-                    d.documenttype = 'specimen'
-                    d.scientific_name_group = scigroup 
-                    logger.info("Record updated: %s" % (rec['unitID']))
-                    counter['updated'] += 1
-                else :
-                    logger.debug("Record not updated: %s" % (rec['unitID']))
-                    counter['skipped'] += 1
+    counter = {'lines':0, 'new':0, 'updated':0, 'skipped':0, 'start': 0, 'end': 0, 'elapsed': 0}
+    line = ""
+
+    def mark_time():
+        if (self.counter['start'] == 0) :
+            self.counter['start'] = timer()
+        self.counter['end'] = timer()
+        self.counter['elapsed'] = (self.counter['end'] - self.counter['start'])
+
+
+    def handle_line():
+        self.counter['lines'] += 1
+        rec = json.loads(self.line.strip())
+        json_hash = md5(self.line.encode('utf-8').strip()).hexdigest()
+        scigroup = rec['identifications'][0]['scientificName']['scientificNameGroup']
+
+        document = Document.get(record_id = rec['unitID'])
+        if (document):
+            if (json_hash != d.hash) :
+                document.hash = json_hash
+                document.source = rec['sourceSystem']['code']
+                document.record = line
+                document.status = 'Updated'
+                document.nds_index = 'specimen'
+                document.documenttype = 'specimen'
+                document.scientific_name_group = scigroup 
+                logger.info("Record updated: %s" % (rec['unitID']))
+                logger.info("Execution time %0.2f seconds" % (self.counter['elapsed']))
+                self.counter['updated'] += 1
             else :
-                try:
-                    d = Document(
-                            record_id = rec['unitID'],
-                            hash = json_hash,
-                            source = rec['sourceSystem']['code'],
-                            record = line,
-                            status = 'New',
-                            nds_index = 'specimen',
-                            documenttype = 'specimen',
-                            scientific_name_group = scigroup 
-                    )
-                    logger.info("New record stored: %s" % (rec['unitID']))
-                    counter['new'] += 1
-                except:
-                    logger.error("Record already stored: %s" % (rec['unitID']))
-                    counter['skipped'] += 1
-    end = timer()
+                logger.debug("Record not updated: %s" % (rec['unitID']))
+                self.counter['skipped'] += 1
+        else :
+            try:
+                document = Document(
+                    record_id = rec['unitID'],
+                    hash = json_hash,
+                    source = rec['sourceSystem']['code'],
+                    record = line,
+                    status = 'New',
+                    nds_index = 'specimen',
+                    documenttype = 'specimen',
+                    scientific_name_group = scigroup 
+                )
+                logger.info("New record stored: %s" % (rec['unitID']))
+                self.counter['new'] += 1
+            except:
+                logger.error("Record already stored: %s" % (rec['unitID']))
+                self.counter['skipped'] += 1
 
-    logger.info("Execution time %0.2f seconds" % (end-start))
-    logger.info("%d lines processed" % (counter['lines']))
-    logger.info("%d new records" % (counter['new']))
-    logger.info("%d records updated" % (counter['updated']))
-    logger.info("%d records skipped" % (counter['skipped']))
+
+    @db_session
+    def parse_json(filename):
+        self.mark_time()
+        with open(filename, 'r') as f:
+            for self.line in f:
+                self.handle_line()
+                self.mark_time()
+        self.report()
+
+    def report():
+        logger.info("Execution time %0.2f seconds" % (self.counter['elapsed']))
+        logger.info("%d lines processed" % (self.counter['lines']))
+        logger.info("%d new records" % (self.counter['new']))
+        logger.info("%d records updated" % (self.counter['updated']))
+        logger.info("%d records skipped" % (self.counter['skipped']))
                 
 parse_json('/data/validation-output-20180626-104522--000.json');
